0ffd0a956b35038b3c01136b761c7b8040244469
Austin Bingham <austin.bingham@gmail.com>
2018-02-12 11:49:14 +0100

    Seems to work...

--- server/src/index.js
+++ server/src/index.js
@@ -22,26 +22,11 @@ var term,
     socket,
     pid;
 
-var terminalContainer = document.getElementById('terminal-container')
-    // actionElements = {
-    //     findNext: document.querySelector('#find-next'),
-    //     findPrevious: document.querySelector('#find-previous')
-    // },
-    // optionElements = {
-    //     cursorBlink: document.querySelector('#option-cursor-blink'),
-    //     cursorStyle: document.querySelector('#option-cursor-style'),
-    //     scrollback: document.querySelector('#option-scrollback'),
-    //     tabstopwidth: document.querySelector('#option-tabstopwidth'),
-    //     bellStyle: document.querySelector('#option-bell-style')
-    // }
-// ,
-    // colsElement = document.getElementById('cols'),
-    // rowsElement = document.getElementById('rows')
-;
+var terminalContainer = document.getElementById('terminal-container') ;
 
 function setTerminalSize() {
-    var cols = 70; // parseInt(colsElement.value, 10);
-    var rows = 24; // parseInt(rowsElement.value, 10);
+    var cols = 70;
+    var rows = 24;
     var viewportElement = document.querySelector('.xterm-viewport');
     var scrollBarWidth = viewportElement.offsetWidth - viewportElement.clientWidth;
     var width = (cols * term.renderer.dimensions.actualCellWidth + 20 /*room for scrollbar*/).toString() + 'px';
@@ -51,38 +36,6 @@ function setTerminalSize() {
     terminalContainer.style.height = height;
     term.resize(cols, rows);
 }
-
-// colsElement.addEventListener('change', setTerminalSize);
-// rowsElement.addEventListener('change', setTerminalSize);
-
-// actionElements.findNext.addEventListener('keypress', function (e) {
-//   if (e.key === "Enter") {
-//     e.preventDefault();
-//     term.findNext(actionElements.findNext.value);
-//   }
-// });
-// actionElements.findPrevious.addEventListener('keypress', function (e) {
-//   if (e.key === "Enter") {
-//     e.preventDefault();
-//     term.findPrevious(actionElements.findPrevious.value);
-//   }
-// });
-
-// optionElements.cursorBlink.addEventListener('change', function () {
-//   term.setOption('cursorBlink', optionElements.cursorBlink.checked);
-// });
-// optionElements.cursorStyle.addEventListener('change', function () {
-//   term.setOption('cursorStyle', optionElements.cursorStyle.value);
-// });
-// optionElements.bellStyle.addEventListener('change', function () {
-//   term.setOption('bellStyle', optionElements.bellStyle.value);
-// });
-// optionElements.scrollback.addEventListener('change', function () {
-//   term.setOption('scrollback', parseInt(optionElements.scrollback.value, 10));
-// });
-// optionElements.tabstopwidth.addEventListener('change', function () {
-//   term.setOption('tabStopWidth', parseInt(optionElements.tabstopwidth.value, 10));
-// });
 
 createTerminal();
 
@@ -99,96 +52,19 @@ function createTerminal() {
     term.fit();
     term.focus();
 
-    // term.on('resize', function (size) {
-    //     if (!pid) {
-    //         return;
-    //     }
-    //     var cols = size.cols,
-    //         rows = size.rows,
-    //         url = '/repls/' + pid + '/size?cols=' + cols + '&rows=' + rows + '&avatar_name=' + avatar_name;
-
-    //     fetch(url, {method: 'POST'});
-    // });
-
     protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
     socketURL = protocol + '//' + location.hostname + ((location.port) ? (':' + location.port) : '') + '/repls/';
 
-    // fit is called within a setTimeout, cols and rows need this.
-    setTimeout(function () {
-
-        // Set terminal size again to set the specific dimensions on the demo
-        // setTerminalSize();
-
-        // Request creation of new repl
-        fetch('/repls', {method: 'POST'}).then(function (res) {
-
-            res.text().then(function (processId) {
-                pid = processId;
-                socketURL += processId;
-                socket = new WebSocket(socketURL);
-                socket.onopen = runRealTerminal;
-                // socket.onclose = runFakeTerminal;
-                // socket.onerror = runFakeTerminal;
-            });
+    // Request creation of new repl
+    fetch('/repls', {method: 'POST'}).then(function (res) {
+        res.text().then(function (processId) {
+            pid = processId;
+            socketURL += processId;
+            socket = new WebSocket(socketURL);
+            socket.onopen = function() {
+                term.attach(socket);
+                term._initialize = true;
+            }
         });
-    }, 0);
-    // // fit is called within a setTimeout, cols and rows need this.
-    // setTimeout(function () {
-
-    //     // Set terminal size again to set the specific dimensions on the demo
-    //     // setTerminalSize();
-    //             // socket.on('connect', runRealTerminal);
-    //     // socket.on('disconnect', runFakeTerminal);
-    //     // socket.on('event', runFakeTerminal);
-    // }, 0);
-}
-
-function runRealTerminal() {
-    console.log('connecting terminal');
-    socket.onmessage = function (event) {
-        console.log(event.data);
-    }
-    term.attach(socket);
-    term._initialized = true;
-}
-
-function runFakeTerminal() {
-    // if (term._initialized) {
-    //     return;
-    // }
-
-    // term._initialized = true;
-
-    // var shellprompt = '$ ';
-
-    // term.prompt = function () {
-    //     term.write('\r\n' + shellprompt);
-    // };
-
-    // term.writeln('Welcome to xterm.js');
-    // term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
-    // term.writeln('Type some keys and commands to play around.');
-    // term.writeln('');
-    // term.prompt();
-
-    // term.on('key', function (key, ev) {
-    //     var printable = (
-    //         !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
-    //     );
-
-    //     if (ev.keyCode == 13) {
-    //         term.prompt();
-    //     } else if (ev.keyCode == 8) {
-    //         // Do not delete the prompt
-    //         if (term.x > 2) {
-    //             term.write('\b \b');
-    //         }
-    //     } else if (printable) {
-    //         term.write(key);
-    //     }
-    // });
-
-    // term.on('paste', function (data, ev) {
-    //     term.write(data);
-    // });
+    });
 }
--- server/src/server.py
+++ server/src/server.py
@@ -30,31 +30,10 @@ async def kill_all_repls(app):
 
 app.on_shutdown.append(kill_all_repls)
 
+
 async def index(request):
     with open('src/static/index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
-
-
-async def send_data(sid, lines):
-    for line in lines:
-        await sio.emit(
-            'data',
-            line.decode('utf-8'),
-            room=sid,
-            namespace='/repls')
-
-
-# async def emit_messages():
-#     while True:
-#         sid, line = await message_queue.get()
-#         print('emiting:', line)
-#         await sio.emit(
-#             'data',
-#             line.decode('utf-8'),
-#             room=sid,
-#             namespace='/repls')
-
-# loop.create_task(emit_messages())
 
 
 class ReplProtocol(asyncio.SubprocessProtocol):
@@ -64,9 +43,6 @@ class ReplProtocol(asyncio.SubprocessProtocol):
 
     def pipe_data_received(self, fd, data):
         self.queue.put_nowait(data)
-
-    def process_exited(self):
-        print('process exited')
 
 
 async def create_repl(request):
@@ -105,10 +81,9 @@ async def websocket_handler(request):
     loop.create_task(
         process_repl_output(ws, protocol.queue))
 
-    while True:
-        msg = await ws.receive()
-        print('repl input:', msg)
+    async for msg in ws:
         data = msg.data.encode('utf-8').replace(b'\r', b'\n')
+        ws.send_str(msg.data.replace('\r', '\r\n'))
         transport.get_pipe_transport(0).write(data)
 
     await ws.close()
@@ -132,122 +107,3 @@ if __name__ == '__main__':
         aiohttp_autoreload.start()
 
     web.run_app(app)
-
-
-# @app.route("/")
-# def index():
-#     return app.send_static_file('index.html')
-
-
-# def repl_read(avatar_name, fd):
-#     print('repl_read: {}'.format(avatar_name))
-#     data = os.read(fd, 1024)
-
-#     print('read:', data)
-
-#     try:
-#         socket = sockets[avatar_name]
-#         socket.write(data)
-#     except KeyError:
-#         pass
-
-#     assert avatar_name in logs
-#     logs[avatar_name] += data
-
-#     return data
-
-
-# @socketio.on('connect')
-# def handle_connect():
-#     print('connect! {}'.format(request.args))
-
-#     try:
-#         avatar_name = request.args['avatar_name']
-#         # cols = int(request.args['cols'])
-#         # rows = int(request.args['rows'])
-#     except KeyError:
-#         # TODO: return appropriate HTTP error
-#         raise
-#     except ValueError:
-#         # TODO: Raise appropriate HTTP error...int conversion has failed
-#         raise
-
-#     if avatar_name in repls:
-#         old_repl = repls[avatar_name]
-#         old_repl.kill()
-#         print('Closed REPL:', old_repl.pid/sizee
-#         del repls[avatar_name]
-#         del logs[avatar_name]
-
-#     repl = subprocess.Popen('python',
-#                             stdin=subprocess.PIPE,
-#                             stdout=subprocess.PIPE,
-#                             stderr=subprocesss.STDOUT)
-#     repls[avatar_name] = repl
-# #         repl = pty.spawn(
-# #             'python', [], {
-# #                 name: 'xterm-color',  // Sets the TERM environment variable, so
-# #                 // must be 'xterm' or 'xterm-color'
-# #                 cols: cols || 80,
-# #                 rows: rows || 24,
-# #                 cwd: process.env.PWD,
-# #                 env: process.env
-# #             }),
-
-#     print('Created Python REPL with PID:', repl.pid)
-#     repls[avatar_name] = repl
-#     logs[avatar_name] = ''
-
-#     sel.register(repl.stdout, selectors.EVENT_READ, lambda data: repl_read(avatar_name, data))
-
-#     # resp = jsonify({'repl_pid': repl.pid})
-#     # resp.status_code = 201
-
-#     # return resp
-
-
-# @socketio.on('data')
-# def handle_message(message):
-#     print('received data: ', message)
-#     repl = repls[message['avatar_name']]
-#     repl.stdin.write(message['data'].encode('utf-8'))
-#     repl.stdin.flush()
-
-
-# @socketio.on('json')
-# def handle_json(json):
-#     print('received json: ' + str(json))
-
-# # // Websocket for interacting with REPL
-# # app.ws('/repls/:kata_id/:avatar_name', function (ws, req) {
-# #     var repl_key = req.params.kata_id + "/" + req.params.avatar_name;
-# #     var repl = repls[repl_key];
-# #     console.log('Connected to terminal ' + repl.pid);
-# #     ws.send(logs[repl.pid]);
-
-# #     repl.on('data', function(data) {
-# #         try {
-# #             ws.send(data);
-# #         } catch (ex) {
-# #             // The WebSocket is not open, ignore
-# #         }
-# #     });
-# #     ws.on('message', function(msg) {
-# #         repl.write(msg);
-# #     });
-# #     ws.on('close', function () {
-# #         repl.kill();
-# #         console.log('Closed terminal ' + repl.pid);
-# #         // Clean things up
-# #         delete repls[repl_key];
-# #         delete logs[repl_key];
-# #     });
-# # });
-
-
-# if __name__ == '__main__':
-#     host = '127.0.0.1' if os.name == 'nt' else '0.0.0.0'
-#     port = os.environ.get('PORT', 3000)
-
-#     print('App listening to http://{}:{}'.format(host, port))
-#     socketio.run(app, host=host, port=port)
