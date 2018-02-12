a03838af60538a2023e97be513326ba704ab055c
Austin Bingham <austin.bingham@gmail.com>
2018-02-12 12:28:18 +0100

    Using PTY.
    This results in far less mucking around with line endings.

--- server/src/server.py
+++ server/src/server.py
@@ -1,32 +1,23 @@
 import asyncio
-import functools
-import json
 import os
+import pty
 
 from aiohttp import web
-import socketio
 
 
 repls = {}
 logs = {}
 queues = {}
 
-sio = socketio.AsyncServer()
 app = web.Application()
-sio.attach(app)
 loop = asyncio.get_event_loop()
 
+
 async def kill_all_repls(app):
-    # global repls
-    # for transport, protocol in repls.values():
-    #     if transport.get_returncode() is not None:
-    #         try:
-    #             print('killing repl:', transport.get_pid())
-    #             transport.kill()
-    #         except ProcessLookupError:
-    #             pass
-    # repls = {}
-    pass
+    global repls
+    for proc, fd, queue in repls.values():
+        proc.kill()
+    repls = {}
 
 app.on_shutdown.append(kill_all_repls)
 
@@ -36,58 +27,47 @@ async def index(request):
         return web.Response(text=f.read(), content_type='text/html')
 
 
-class ReplProtocol(asyncio.SubprocessProtocol):
-    def __init__(self):
-        super().__init__()
-        self.queue = asyncio.Queue()
-
-    def pipe_data_received(self, fd, data):
-        self.queue.put_nowait(data)
-
-
 async def create_repl(request):
-    print('creating repl')
-    transport, protocol = await loop.subprocess_exec(
-        lambda: ReplProtocol(),
-        'python', '-i', '-u',
-        )
+    """Start a new REPL accessed through a PTY.
+    """
+    master, slave = pty.openpty()
 
-    # proc is a tuple of (transport, protocol)
-    repls[transport.get_pid()] = (transport, protocol)
+    queue = asyncio.Queue()
+    proc = await asyncio.create_subprocess_exec(
+        'python',
+        stdin=slave,
+        stdout=slave,
+        stderr=slave)
+    loop.add_reader(master, lambda: queue.put_nowait(os.read(master, 1024)))
+
+    repls[proc.pid] = (proc, master, queue)
     return web.Response(
-        text=str(transport.get_pid()))
+        text=str(proc.pid))
 
 
 async def process_repl_output(ws, queue):
     while True:
         data = await queue.get()
         print('repl output:', data)
-        data = data.replace(b'\n', b'\r\n')
         ws.send_str(data.decode('utf-8'))
-
-    # TODO: Need to properly handle ws shutdown? Or will that be handled
-    # properly by the loop?
 
 
 async def websocket_handler(request):
     pid = int(request.match_info['pid'])
-    print('pid:', pid)
 
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
+    socket = web.WebSocketResponse()
+    await socket.prepare(request)
 
-    transport, protocol = repls[pid]
+    proc, repl_fd, queue = repls[pid]
 
     loop.create_task(
-        process_repl_output(ws, protocol.queue))
+        process_repl_output(socket, queue))
 
-    async for msg in ws:
-        data = msg.data.encode('utf-8').replace(b'\r', b'\n')
-        ws.send_str(msg.data.replace('\r', '\r\n'))
-        transport.get_pipe_transport(0).write(data)
+    async for msg in socket:
+        os.write(repl_fd, msg.data.encode('utf-8'))
 
-    await ws.close()
-    return ws
+    await socket.close()
+    return socket
 
 
 app.router.add_static('/static', 'src/static')
