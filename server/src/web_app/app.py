5e44412f0150af707360496733cbd605398cc2f8
Austin Bingham <austin.bingham@gmail.com>
2018-02-26 15:15:34 +0100

    Added an "is_alive" endpoint for client polling.

--- server/src/web_app/app.py
+++ server/src/web_app/app.py
@@ -1,6 +1,14 @@
 from sanic import Sanic
+import sanic.response
 
 from .handlers import Handlers
+
+
+def is_alive(request):
+    """Just a simple endpoint that a client can use to determine if this server is
+    running.
+    """
+    return sanic.response.HTTPResponse(status=200)
 
 
 def run(host, port, log_config):
@@ -14,5 +22,7 @@ def run(host, port, log_config):
     app.add_route(handlers.create_repl_handler, '/', methods=['POST'])
     app.add_route(handlers.delete_repl_handler, '/',  methods=['DELETE'])
     app.add_websocket_route(handlers.websocket_handler, '/')
+
+    app.add_route(is_alive, "/is_alive")
 
     app.run(host=host, port=port)
