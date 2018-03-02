fb1b259e37224f88ca105f29c12457b2ba13a0a2
Austin Bingham <austin.bingham@gmail.com>
2018-03-02 10:25:02 +0100

    Updated tests to account for pytest-sanic working now.

--- server/test/test_repl_container.py
+++ server/test/test_repl_container.py
@@ -7,12 +7,10 @@ async def test_is_alive_responds_with_200(test_cli):
 
 
 class TestReplCreation:
-    @pytest.mark.xfail(reason="sanic-pytest issue #13")
     async def test_repl_creation_responds_with_201(self, test_cli):
         resp = await test_cli.post('/', data=b'[]')
         assert resp.status == 201
 
-    @pytest.mark.xfail(reason="sanic-pytest issue #13")
     async def test_repeated_repl_creations_responds_with_409(self, test_cli):
         await test_cli.post('/', data=b'[]')
         resp = await test_cli.post('/', data=b'[]')
@@ -24,7 +22,6 @@ class TestReplDeletion:
         resp = await test_cli.delete('/')
         assert resp.status == 404
 
-    @pytest.mark.xfail(reason="sanic-pytest issue #13")
     async def test_repl_deletion_with_active_repl_responds_with_200(self, test_cli):
         await test_cli.post('/', data=b'[]')
         resp = await test_cli.delete('/')
