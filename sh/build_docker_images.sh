3934a751a69c917603e7cb7a3edf63d03fa482af
Austin Bingham <austin.bingham@gmail.com>
2018-03-02 10:12:38 +0100

    Split image into "deps" and "source" images.
    The deps image is slow to build but changes infrequently. THe source images
    builds quickly and changes a lot.

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,4 +2,5 @@
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+docker build -t cyberdojo/repl_container_python_base -f ${ROOT_DIR}/server/Dockerfile.base ${ROOT_DIR}/server
 docker-compose --file ${ROOT_DIR}/docker-compose.yml build
